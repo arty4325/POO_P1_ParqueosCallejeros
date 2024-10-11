1e76254b76241ab740fe2f0e314b6fbd7b6b28e2
Oscar Arturo Acuña Durán <53323325+arty4325@users.noreply.github.com>
2024-10-10 23:43:38 -0600

    Pequenos cambios para poder cambiar de aplicacion

--- ParqueosCallejeros/src/main/java/com/example/parqueoscallejeros/HelloApplication.java
+++ ParqueosCallejeros/src/main/java/com/example/parqueoscallejeros/HelloApplication.java
@@ -8,15 +8,41 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
 public class HelloApplication extends Application {
+
+    private static Stage primaryStage; // Guardar una referencia al Stage principal
+
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/parqueoscallejeros/User/UserLogin/Scene1.fxml"));
+        primaryStage = stage;
+        showUsuarios(); // Iniciar con la primera escena
+    }
+
+    // Método para mostrar la primera escena
+    public static void showAdministradores() throws IOException {
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("/com/example/parqueoscallejeros/User/UserLogin/Scene1.fxml"));
         Scene scene = new Scene(root);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("Scene 1");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    // Método para mostrar la segunda escena
+    public static void showUsuarios() throws IOException {
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("/com/example/parqueoscallejeros/User/UserLogin/Scene1.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Scene 2");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    // Método para mostrar la tercera escena
+    public static void showInspectores() throws IOException {
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("/com/example/parqueoscallejeros/User/UserLogin/Scene3.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Scene 3");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
